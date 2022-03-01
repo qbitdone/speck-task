7d9e0baba6d133efab8763f4afbbae38c238399c
Kristijan <kculina@protonmail.ch>
2022-03-01 21:14:56 +0100

    made responsive header

---
+++ js/scroll.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    var showHeaderAt = 150;
+
+    var win = $(window),
+            body = $('body');
+
+    // Show the fixed header only on larger screen devices
+
+    if(win.width() > 400){
+
+        // When we scroll more than 150px down, we set the
+        // "fixed" class on the body element.
+
+        win.on('scroll', function(e){
+
+            if(win.scrollTop() > showHeaderAt) {
+                body.addClass('fixed');
+            }
+            else {
+                body.removeClass('fixed');
+            }
+        });
+
+    }
+
+});
